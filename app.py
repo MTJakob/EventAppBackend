b72c398d1a806e7137ee785d54e9ddd742b54847
MichalJakob <01131065@pw.edu.pl>
2024-01-16 16:59:03 +0100

    app.py file is modified to adapt abstract factory design pattern

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from flask import Flask
-from flask_restful import Api
-import os
+from flask_smorest import Api
 from database import db
+from resources.Event import blp as EventBlueprint
+from resources.TestFunctions import blp as TestBlueprint
+import os
 
 
 def create_app(db_url=None):
     app = Flask(__name__)
+    app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Event Manager REST API"
     app.config["API_VERSION"] = "v1"
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     # disable for development purpose, should be switched on production env
     # app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
@@ -17,16 +24,13 @@ def create_app(db_url=None):
     with app.app_context():
         db.create_all()
 
+    api.register_blueprint(EventBlueprint)
+    api.register_blueprint(TestBlueprint)
+
     return app
 
 
 app = create_app()
 
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'API for Flutter Application!'
-
-
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
