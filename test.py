298802c4f9888a7cde24c4c44c82e41b7155232c
MichalJakob <01131065@pw.edu.pl>
2024-01-27 14:02:17 +0100

    test.py is deleted due change in API development endpoint tester to Swagger-UI

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-BASE = "http://127.0.0.1:5000/"
-
-# response = requests.get(BASE + "test3")
-# print(response.json())
-
-response = requests.post(BASE + "test4", {'filename': "result.json"})
-print(response)
