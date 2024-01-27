d2c908603620770a555ac71e876c42c8906b67f6
MichalJakob <01131065@pw.edu.pl>
2024-01-27 14:09:03 +0100

    Unifed response message structure is added to login endpoint

--- resources/Login.py
+++ resources/Login.py
@@ -7,7 +7,7 @@ from schemas.argumentSchemas import LoginSchema
 from flask_jwt_extended import create_access_token
 import os
 
-blp = Blueprint("Login", __name__, description="Endpoint on login")
+blp = Blueprint("Login", __name__, description="Endpoint for login")
 
 crypt_context = CryptContext(schemes=[os.getenv("CRYPT_CONTEXT")])
 
@@ -20,5 +20,6 @@ class User(MethodView):
         user = UserTable.query.filter_by(Email=user_data["Email"]).one_or_404()
         if crypt_context.verify(user_data["Password"], user.Password):
             access_token = create_access_token(identity=user.IDUser)
-            return {"access_token": access_token}, 200
+            return {"message": "User logged in successfully",
+                    "access_token": access_token}, 200
         abort(401, message="Invalid email or password.")
