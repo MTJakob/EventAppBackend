b164322f80855ac0d2ba00658049485bb98bbad2
MichalJakob <01131065@pw.edu.pl>
2024-01-27 17:14:39 +0100

    Event post and get endpoint are created

--- resources/Event.py
+++ resources/Event.py
@@ -1,50 +1,58 @@
-from flask_restful import Api, Resource, abort, fields, marshal_with, reqparse
-from flask import request
-from database import db
+from flask_restful import abort
+from flask.views import MethodView
+from database import db, User, Event as EventTable
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 from flask_smorest import Blueprint, abort
+from schemas.PlainSchemas import EventSchema
+from schemas.argumentSchemas import EventPostSchema, EventDeleteSchema
+from schemas.argumentSchemas import EventGetSchema
+from datetime import datetime
 
 blp = Blueprint("Event", __name__, description="Operations on events")
 
-#
-# class Event(Resource):
-#     def __init__(self):  # This method request pre-defined JSON file
-#         parser = reqparse.RequestParser()
-#         parser.add_argument("Name", type=str, help="User name", required=True)
-#         parser.add_argument("Surname", type=str, help="User Surname", required=True)
-#         parser.add_argument("Email", type=str, help="User Email", required=True)
-#         parser.add_argument("DateOfBirth", type=str, help="User Surname", required=True)
-#         parser.add_argument("Password", type=str, help="User Surname", required=True)
-#         self.req_parser = parser
 
+@blp.route('/event/<string:user_id>')
+class Event(MethodView):
 
-@blp.route('/event')
-def post(self):
-    request_data = request.getjson()
-    name = self.req_parser.parse_args(strict=True).get("Name", None)
-    surname = self.req_parser.parse_args(strict=True).get("Surname", None)
-    email = self.req_parser.parse_args(strict=True).get("Email", None)
-    date_of_birth = self.req_parser.parse_args(strict=True).get("DateOfBirth", None)
-    password = self.req_parser.parse_args(strict=True).get("Password", None)
+    @blp.arguments(EventPostSchema)
+    def post(self, user_data, user_id):
+        event = EventTable \
+        (Name=user_data["Name"],
+         Price=user_data["Price"],
+         StartDateTime=user_data["StartDateTime"],
+         EndDateTime=user_data["EndDateTime"],
+         Capacity=user_data["Capacity"],
+         IDOrganiser=user_id
+         )
+        try:
+            db.session.add(event)
+            db.session.commit()
+        except IntegrityError:
+            abort(400, message="Submitted event cannot be added due to lack of data integrity")
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while inserting the event.")
+        return {"message": "Event created successfully"}, 201
 
-    #new_user = User(name, surname, email, date_of_birth, password)
-    try:
-        db.session.add(new_user)
-        db.session.commit()
-    except IntegrityError:
-        abort(400, message="A store with that name already exists.",)
-    except SQLAlchemyError:
-        abort(500, message="An error occurred while inserting the item.")
+    #@blp.response(200, AddressSchema(many=True))
+    def put(self):
+        p = EventTable(Name='TestEventIDCategory0', Capacity=60,
+                       StartDateTime=datetime.now(),
+                       EndDateTime=datetime.now(),
+                       Price=40.0, IDCategory=0)
+        #p = Category(Name="Sporty i Rekreacje")
+        try:
+            db.session.add(p)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while inserting the item.")
+        return "OK", 201
 
-    return "User is register successfully", 201
+    @blp.response(200, EventSchema(many=True))
+    def get(self, user_id):
+        data = EventTable.query.all()  # .filter(EventParticipant.IDUser == user_id)
+        return data, 200
 
+    @blp.arguments(EventDeleteSchema)
+    def delete(self):
 
-@blp.route("/event/put")
-def put():
-    return "OK", 201
-
-
-@blp.route("/event/get")
-def get():
-    #data = Address.query.get_or_404(1)
-    return data, 200
+        return "OK"
