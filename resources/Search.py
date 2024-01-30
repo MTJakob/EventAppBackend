62265ea8d30790d2f9a9356f6fc595da75ce8e3e
MichalJakob <01131065@pw.edu.pl>
2024-01-30 18:04:55 +0100

    Search endpoint is created

---
+++ resources/Search.py
@@ -0,0 +1,20 @@
+from flask_restful import abort
+from flask.views import MethodView
+from database import db, Event as EventTable
+from sqlalchemy.exc import SQLAlchemyError
+from flask_smorest import Blueprint
+from schemas.argumentSchemas import SearchPostSchema
+from schemas.responseSchemas import EventGetSchema
+
+blp = Blueprint("Search", __name__, description="Endpoint for searching the events")
+
+
+@blp.route("/search")
+class Search(MethodView):
+
+    @blp.arguments(SearchPostSchema)
+    @blp.response(200, EventGetSchema(many=True))
+    def post(self, user_data):
+        search_word = "%{}%".format(user_data["SearchWord"])
+        events = EventTable.query.filter(EventTable.Name.like(search_word)).all()
+        return events, 200
