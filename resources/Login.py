c19844772bfd27c600958576e510eefe1b19dcfc
MichalJakob <01131065@pw.edu.pl>
2024-01-30 13:04:22 +0100

    Login PUT method error messages are corrected

--- resources/Login.py
+++ resources/Login.py
@@ -29,10 +29,12 @@ class User(MethodView):
     @blp.arguments(ChangePasswordSchema)
     def put(self, user_data):
         user = UserTable.query.filter_by(IDUser=user_data["IDUser"]).one_or_404()
-        if crypt_context.verify(user_data["OldPassword"], user.Password):
-            user.Password = crypt_context.hash(user_data["NewPassword"])
+        if not crypt_context.verify(user_data["OldPassword"], user.Password):
+            abort(401, message="Invalid old password.")
+        user.Password = crypt_context.hash(user_data["NewPassword"])
         try:
             db.session.commit()
         except SQLAlchemyError:
             abort(500, message="An error occurred while changing the password.")
-        abort(401, message="Invalid old password.")
+        return {"message": "User password updated successfully"}, 200
+
