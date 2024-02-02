fae6cdbe9a5d184ade289b8108729516c8cf7d0f
MichalJakob <01131065@pw.edu.pl>
2024-02-02 22:05:58 +0100

    EventGetSchema is updated with is signed in fields

--- schemas/responseSchemas.py
+++ schemas/responseSchemas.py
@@ -1,9 +1,17 @@
 from marshmallow import fields
-from schemas.PlainSchemas import PlainEventSchema, PlainUserSchema, PlainCategorySchema, PlainAddressSchema
+from schemas.PlainSchemas import PlainEventSchema, PlainUserSchema, PlainCategorySchema, PlainAddressSchema,\
+    PlainEventParticipantSchema
 
 
 class EventGetSchema(PlainEventSchema):
     eventUser = fields.Nested(PlainUserSchema(exclude=("Email", "DateOfBirth",)), dump_only=True)
     eventCategory = fields.Nested(PlainCategorySchema(only=("Name",)), dump_only=True)
     eventAddress = fields.Nested(PlainAddressSchema(exclude=("IDAddress", "Name")), dump_only=True)
+    eventParticipantEventFrom = fields.Nested(PlainEventParticipantSchema(exclude=("Review",)), dump_only=True)
+    is_singed = fields.Method("is_signed_up_for_the_event", dump_only=True)
+    is_singed_lambda = fields.Function(lambda obj: not obj.eventParticipantEventFrom, dump_only=True)
 
+    def is_signed_up_for_the_event(self, obj):
+        if bool(obj.eventParticipantEventFrom):
+            return True
+        return False
