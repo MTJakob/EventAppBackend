65c9e0668e7f51bd92a9475c3200bac4325bbd23
MichalJakob <01131065@pw.edu.pl>
2024-02-05 22:28:25 +0100

    is_signed value is suspended for development porpoise

--- schemas/responseSchemas.py
+++ schemas/responseSchemas.py
@@ -7,11 +7,11 @@ class EventGetSchema(PlainEventSchema):
     eventUser = fields.Nested(PlainUserSchema(exclude=("Email", "DateOfBirth",)), dump_only=True)
     eventCategory = fields.Nested(PlainCategorySchema(only=("Name",)), dump_only=True)
     eventAddress = fields.Nested(PlainAddressSchema(exclude=("IDAddress", "Name")), dump_only=True)
-    eventParticipantEventFrom = fields.Nested(PlainEventParticipantSchema(exclude=("Review",)), dump_only=True)
-    is_singed = fields.Method("is_signed_up_for_the_event", dump_only=True)
-    is_singed_lambda = fields.Function(lambda obj: not obj.eventParticipantEventFrom, dump_only=True)
-
-    def is_signed_up_for_the_event(self, obj):
-        if bool(obj.eventParticipantEventFrom):
-            return True
-        return False
+    # eventParticipantEventFrom = fields.Nested(PlainEventParticipantSchema(exclude=("Review",)), dump_only=True)
+    # is_singed = fields.Method("is_signed_up_for_the_event", dump_only=True)
+    # is_singed_lambda = fields.Function(lambda obj: not obj.eventParticipantEventFrom, dump_only=True)
+    #
+    # def is_signed_up_for_the_event(self, obj):
+    #     if bool(obj.eventParticipantEventFrom):
+    #         return True
+    #     return False
